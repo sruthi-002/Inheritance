055f27dbac6b84388ff0b7ff018cd93fbbba8425
sruthi-002 <89520373+sruthi-002@users.noreply.github.com>
2022-07-28 16:20:09 +0530

    Add files via upload

---
+++ Inheritance.java
@@ -0,0 +1,15 @@
+﻿package com.company;
+import java.util.Scanner;
+class Student {
+        static String name = "Sruthi";
+}
+class Main extends Student{
+    public static void main(String [] args)
+    {
+        int tamil;
+        Scanner in = new Scanner(System.in);
+        tamil = in.nextInt();
+        Main m = new Main();
+        System.out.println(name + " "+"got " + " "+ tamil);
+    }
+}
